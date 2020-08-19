978b9eb61a34ff8020f56f6a1b8af01d8bbfafd7
alexalemanhdz <alex.aleman.public@gmail.com>
2020-08-19 18:35:05 -0500

    Activated I guess

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const Token = require('./token');
 
 const client = new Discord.Client();
 const token = Token.token;
+const regex = /^price\s/
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('I am ready!');
 });
+
+client.on('message', message => {
+  if (message.content.match(regex)) {
+    message.channel.send('I\'m working on that. 🛠️');
+  }
+})
 
 client.login(token);
