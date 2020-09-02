b9f7addb3d15bbaf94c36153b9e76e7d2071b64f
alexalemanhdz <alex.aleman.public@gmail.com>
2020-09-02 18:36:30 -0500

    good enough

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const https = require('https');
 const cron = require('node-cron');
 const Discord = require('discord.js');
-const Token = require('./token');
-const Coins = require('./coins');
-const Channel = require('./channel');
+const token = require('./token').token;
+const coins = require('./coins').coins;
+const localCurrencies = require('./localCurrencies').localCurrencies;
+const channelId = require('./channel').channel;
+
+const OPENING = 'Opening';
+const CLOSING = 'Closing';
 
 const client = new Discord.Client();
-const token = Token.token;
-const coins = Coins.coins;
-const channelStr = Channel.channel;
 const regex = /^price\s/
-let biz = null;
+let channelName = null;
 
-const coinRequest = (coin, channel) => {
+const coinRequest = (coin, channel, label = 'Current') => {
   https.get({
     host: 'api.coingecko.com',
     path: `/api/v3/coins/${coin}`,
@@ -27,11 +28,20 @@ const coinRequest = (coin, channel) => {
         rawData += d;
       });
       res.on('end', () => {
-        const data = JSON.parse(rawData);
-        const usdPrice = data.market_data.current_price.usd;
-        const mxnPrice = data.market_data.current_price.mxn;
+        const data = JSON.parse(rawData);        
         const name = data.name;
-        channel.send(`Current price for ${name} is ${usdPrice} USD or ${mxnPrice} MXN!`);
+
+        localCurrencies.forEach((currency) => {
+          const price = data.market_data.current_price[currency];
+          const formatter = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: currency.toLocaleUpperCase(),
+          });
+
+          if (price) {
+            channel.send(`${label} price for ${name} is ${formatter.format(price)}!`);
+          }
+        });
       });
     }
     else {
@@ -43,7 +53,7 @@ const coinRequest = (coin, channel) => {
 client.on('ready', () => {
   console.log('I am ready!');
   client.user.setActivity('price', { type: 'LISTENING' });
-  biz = client.channels.cache.get(channelStr);
+  channelName = client.channels.cache.get(channelId);
 });
 
 client.on('message', message => {
@@ -54,7 +64,7 @@ client.on('message', message => {
 })
 
 cron.schedule('0 18 * * *', () => {
-  coins.forEach(coinStr => coinRequest(coinStr, biz));
+  coins.forEach(coinStr => coinRequest(coinStr, channelName, CLOSING));
 });
 
 client.login(token);
